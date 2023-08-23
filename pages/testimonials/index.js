65f38fccc4d2b9575f82b83319f2cff1265802f7
YAOSTEPHANE <106774656+YAOSTEPHANE@users.noreply.github.com>
2023-08-23 16:02:36 +0000

    Add files via upload

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,55 +1,31 @@
-
-//components
-import ServiceSlider from '../../components/ServiceSlider';
-import Bulb from '../../components/Bulb';
-import Circles from '../../components/Circles';
+// components
+import TestimonialSlider from '../../components/TestimonialSlider';
 
 
-//framer motion
+//framer motion 
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
-
-const Services = () => {
-  return (
-    <div className='h-full bg-primary/30 py-36 flex items-center'>
-      <Circles />
-      <div className="container  mx-auto">
-        <div className='flex flex-col xl:flex-row gap-x-8'>
-          {/* text */}
-          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4  xl:mb-0'>
-            <motion.h2
-              variants={fadeIn('up', 0.2)}
-              initial='hidden'
-              animate="show"
-              exit="hidden"
-              className='h2 xl:mt-8'>
-              Mes <span
-                className='text-blue-600'>Services</span>.
-            </motion.h2>
-            <motion.p 
-            variants={fadeIn('up', 0.4)}
-              initial='hidden'
-              animate="show"
-              exit="hidden"
-               className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
-              La saison 2023/2024 en Arabie saoudite a commencé avec 
-              plusieurs internationaux africains. La plupart d’entre eux jouait
-               en Europe. Revue d’effectif.
-            </motion.p>
-          </div>
-          <motion.div variants={fadeIn('down', 0.6)}
-              initial='hidden'
-              animate="show"
-              exit="hidden" className='w-full xl:max-w-[65%]'>
-            {/* slider */}
-            <ServiceSlider />
-          </motion.div>
-
-        </div>
-      </div>
-      <Bulb />
+const Testimonials = () => {
+  return ( 
+  <div className='h-full bg-primary/30 py-32 text-center'>
+    <div className='container mx-auto h-full flex flex-col justify-center'>
+      {/* title */}
+      <motion.h2 
+      variants={fadeIn('up', 0.2)} 
+      initial='hidden'  
+      animate='show' exit='hidden'
+      className='h2 mb-8 xl:mb-0'>Qu'est-ce que 
+       <span className='text-accent'> Disent</span> les Clients.</motion.h2>
+      {/* slider */}
+      <motion.div
+      variants={fadeIn('up', 0.4)} 
+      initial='hidden'  
+      animate='show' exit='hidden'>
+        <TestimonialSlider/>
+      </motion.div>
     </div>
+  </div>
   );
 };
 
-export default Services;
+export default Testimonials;
